80c89a69e6adfc6546ea3c1bcdaabb053054a87b
Andrew Garber <andrew@andrewgarber.com>
2011-05-16 22:49:11 -0400

    got blinking working using timer0
    The timer0 counter increments once every 1024 CPU cycles (i.e. its
    prescaler is set to 1/1024). By waiting 125 counts between interrupts,
    one "tick" is equivalent to exactly 8 milliseconds. This is very
    convenient since dividing/multiplying by 8 merely requires a shift by 3.
    
    Note: this has only been tested on an Arduino Duemilanove board.

--- hello_world.c
+++ hello_world.c
@@ -1,23 +1,56 @@
-#include <stdint.h>
-#include <stdbool.h>
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
+#include <stdint.h>
+
+
+// given that timer0 will be prescaled by 1024,
+// let 1 tick equal exactly 8 msec
+#define CYCLES_PER_TICK (125)
+#define MS_TO_TICKS( ms ) ( (ms) >> 3 )
+#define TICKS_TO_MS( ticks ) ( (ticks) << 3 )
+
+static volatile uint32_t ticks_since_boot = 0;
 
 
 int main()
 {
-    bool state = true;
+    DDRB |= _BV( PIN5 ); // set digital pin 13 to output mode
 
-    // set the mode of the LED pins to output
-    DDRB |= _BV( PIN5 );
-    for( ;; state = !state ) {
-        if( state ) {
-            PORTB |= _BV( PIN5 );
-        } else {
-            PORTB &= ~_BV( PIN5 );
-        }
-        _delay_ms( 500 );
-    }
+#if defined( CLKPR )
+    // disable system clock prescaling
+    CLKPR = _BV( CLKPCE );
+    CLKPR = 0;
+#endif
 
+    // enable the TIMER0_COMPA ISR
+    TIMSK0 = _BV( OCIE0A ); // TIMER0_COMPA
+    TIFR0 &= ~_BV( OCF0A ); // clear OCF0A flag
+
+    // normal (counter) mode, prescaled by 1024
+    TCCR0A = 0;
+    TCCR0B = _BV( CS02 ) | _BV( CS00 );
+
+    // set timer to fire after 1 tick
+    OCR0A = TCNT0 + CYCLES_PER_TICK;
+
+    // enable interrupts
+    sei();
+
+    for( ;; ); // loop forever
     return 0;
+}
+
+
+ISR( TIMER0_COMPA_vect )
+{
+    // set timer to fire again after 1 tick
+    OCR0A += CYCLES_PER_TICK;
+
+    ticks_since_boot++;
+
+    // toggle LED every second
+    if( !(ticks_since_boot % MS_TO_TICKS( 1000 )) ) {
+        PORTB ^= _BV( PIN5 ); // flip LED (Arduino's PIN9) state
+    }
 }
