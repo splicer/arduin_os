fd9996013f574b2297ac5ed822ab6760ddf132e0
Andrew Garber <andrew@andrewgarber.com>
2011-05-23 11:13:23 -0400

    fixed atmega328p build

--- hello_world.c
+++ hello_world.c
@@ -121,6 +121,7 @@ void serial_init(void)
 #define BAUD 9600
     // USING u2x!!!
 #define UBRR_SETTING (F_CPU / 4 / (BAUD - 1) / 2)
+#if defined( UBRRH )
     UBRRH = (uint8_t)(UBRR_SETTING >> 8);
     UBRRL = (uint8_t)UBRR_SETTING;
 
@@ -128,17 +129,26 @@ void serial_init(void)
 
     // Enable the receiver and transmitter
     UCSRB = _BV(RXEN) | _BV(TXEN);
+#else
+    UBRR0H = (uint8_t)(UBRR_SETTING >> 8);
+    UBRR0L = (uint8_t)UBRR_SETTING;
+
+    UCSR0A = _BV(U2X0);
+
+    // Enable the receiver and transmitter
+    UCSR0B = _BV(RXEN0) | _BV(TXEN0);
+#endif
 }
 
 
 
-void serial_send(uint8_t c)
+void serial_send( uint8_t c )
 {
-        /* Wait for empty transmit buffer */
-        while ( !( UCSRA & (1<<UDRE)) )
-            ;
-        /* Put data into buffer, sends the data */
-        UDR = c;
+    // Wait for empty transmit buffer
+    while( !( UCSR0A & (1<<UDRE0) ) )
+        ;
+    // Put data into buffer, sends the data
+    UDR0 = c;
 }
 
 
