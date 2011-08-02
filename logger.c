b0376623006eb325413887465b8184b3de869ead
Andrew Garber <andrew@andrewgarber.com>
2011-08-01 22:36:47 -0400

    use macros from atomic.h to make print functions atomic

--- logger.c
+++ logger.c
@@ -1,6 +1,7 @@
 #include "logger.h"
 #include "serial.h"
 #include <avr/io.h>
+#include <util/atomic.h>
 
 #define CYCLES_PER_TICK (125)
 
@@ -37,121 +38,131 @@ void logger_init( volatile uint32_t * _ticks_since_boot )
 
 void print_timestamp()
 {
-    uint32_t abs_time = *ticks_since_boot;
+    ATOMIC_BLOCK( ATOMIC_RESTORESTATE ) {
+        serial_send( 0xfe ); // start packet
 
-    serial_send( 0xfe ); // start packet
+        serial_send( 7 ); // timestamp symbol (not num args)
 
-    serial_send( 7 ); // timestamp symbol (not num args)
-
-    enc_serial_send( (uint8_t)(abs_time >> 24) );
-    enc_serial_send( (uint8_t)(abs_time >> 16) );
-    enc_serial_send( (uint8_t)(abs_time >> 8) );
-    enc_serial_send( (uint8_t)abs_time );
+        enc_serial_send( (uint8_t)((*ticks_since_boot) >> 24) );
+        enc_serial_send( (uint8_t)((*ticks_since_boot) >> 16) );
+        enc_serial_send( (uint8_t)((*ticks_since_boot) >> 8) );
+        enc_serial_send( (uint8_t)(*ticks_since_boot) );
+    }
 }
 
 
 void print0( uint16_t id )
 {
-    uint8_t microtick = TCNT0 + CYCLES_PER_TICK;
-    uint8_t tick = (uint8_t)*ticks_since_boot;
+    ATOMIC_BLOCK( ATOMIC_RESTORESTATE ) {
+        uint8_t microtick = TCNT0 + CYCLES_PER_TICK;
+        uint8_t tick = (uint8_t)*ticks_since_boot;
 
-    serial_send( 0xfe ); // start packet
+        serial_send( 0xfe ); // start packet
 
-    serial_send( 0 ); // num args
-    enc_serial_send( (uint8_t)(id >> 8) );
-    enc_serial_send( (uint8_t)id );
-    enc_serial_send( tick );
-    enc_serial_send( microtick );
+        serial_send( 0 ); // num args
+        enc_serial_send( (uint8_t)(id >> 8) );
+        enc_serial_send( (uint8_t)id );
+        enc_serial_send( tick );
+        enc_serial_send( microtick );
+    }
 }
 
 
 void print1( uint16_t id, uint16_t a )
 {
-    uint8_t microtick = TCNT0 + CYCLES_PER_TICK;
-    uint8_t tick = (uint8_t)*ticks_since_boot;
+    ATOMIC_BLOCK( ATOMIC_RESTORESTATE ) {
+        uint8_t microtick = TCNT0 + CYCLES_PER_TICK;
+        uint8_t tick = (uint8_t)*ticks_since_boot;
 
-    serial_send( 0xfe ); // start packet
+        serial_send( 0xfe ); // start packet
 
-    serial_send( 1 ); // num args
-    enc_serial_send( (uint8_t)(id >> 8) );
-    enc_serial_send( (uint8_t)id );
-    enc_serial_send( tick );
-    enc_serial_send( microtick );
+        serial_send( 1 ); // num args
+        enc_serial_send( (uint8_t)(id >> 8) );
+        enc_serial_send( (uint8_t)id );
+        enc_serial_send( tick );
+        enc_serial_send( microtick );
 
-    enc_serial_send( (uint8_t)(a >> 8) );
-    enc_serial_send( (uint8_t)a );
+        enc_serial_send( (uint8_t)(a >> 8) );
+        enc_serial_send( (uint8_t)a );
+    }
 }
 
 
 void print2( uint16_t id, uint16_t a, uint16_t b )
 {
-    uint8_t microtick = TCNT0 + CYCLES_PER_TICK;
-    uint8_t tick = (uint8_t)*ticks_since_boot;
+    ATOMIC_BLOCK( ATOMIC_RESTORESTATE ) {
+        uint8_t microtick = TCNT0 + CYCLES_PER_TICK;
+        uint8_t tick = (uint8_t)*ticks_since_boot;
 
-    serial_send( 0xfe ); // start packet
+        serial_send( 0xfe ); // start packet
 
-    serial_send( 2 ); // num args
-    enc_serial_send( (uint8_t)(id >> 8) );
-    enc_serial_send( (uint8_t)id );
-    enc_serial_send( tick );
-    enc_serial_send( microtick );
+        serial_send( 2 ); // num args
+        enc_serial_send( (uint8_t)(id >> 8) );
+        enc_serial_send( (uint8_t)id );
+        enc_serial_send( tick );
+        enc_serial_send( microtick );
 
-    enc_serial_send( (uint8_t)(a >> 8) );
-    enc_serial_send( (uint8_t)a );
+        enc_serial_send( (uint8_t)(a >> 8) );
+        enc_serial_send( (uint8_t)a );
 
-    enc_serial_send( (uint8_t)(b >> 8) );
-    enc_serial_send( (uint8_t)b );
+        enc_serial_send( (uint8_t)(b >> 8) );
+        enc_serial_send( (uint8_t)b );
+    }
 }
 
 
 void print3( uint16_t id, uint16_t a, uint16_t b, uint16_t c )
 {
-    uint8_t microtick = TCNT0 + CYCLES_PER_TICK;
-    uint8_t tick = (uint8_t)*ticks_since_boot;
+    ATOMIC_BLOCK( ATOMIC_RESTORESTATE ) {
+        uint8_t microtick = TCNT0 + CYCLES_PER_TICK;
+        uint8_t tick = (uint8_t)*ticks_since_boot;
 
-    serial_send( 0xfe ); // start packet
+        serial_send( 0xfe ); // start packet
 
-    serial_send( 3 ); // num args
-    enc_serial_send( (uint8_t)(id >> 8) );
-    enc_serial_send( (uint8_t)id );
-    enc_serial_send( tick );
-    enc_serial_send( microtick );
+        serial_send( 3 ); // num args
+        enc_serial_send( (uint8_t)(id >> 8) );
+        enc_serial_send( (uint8_t)id );
+        enc_serial_send( tick );
+        enc_serial_send( microtick );
 
-    enc_serial_send( (uint8_t)(a >> 8) );
-    enc_serial_send( (uint8_t)a );
+        enc_serial_send( (uint8_t)(a >> 8) );
+        enc_serial_send( (uint8_t)a );
 
-    enc_serial_send( (uint8_t)(b >> 8) );
-    enc_serial_send( (uint8_t)b );
+        enc_serial_send( (uint8_t)(b >> 8) );
+        enc_serial_send( (uint8_t)b );
 
-    enc_serial_send( (uint8_t)(c >> 8) );
-    enc_serial_send( (uint8_t)c );
+        enc_serial_send( (uint8_t)(c >> 8) );
+        enc_serial_send( (uint8_t)c );
+    }
 }
 
 
 void print4( uint16_t id, uint16_t a, uint16_t b, uint16_t c, uint16_t d )
 {
-    uint8_t microtick = TCNT0 + CYCLES_PER_TICK;
-    uint8_t tick = (uint8_t)*ticks_since_boot;
+    ATOMIC_BLOCK( ATOMIC_RESTORESTATE ) {
+        uint8_t microtick = TCNT0 + CYCLES_PER_TICK;
+        uint8_t tick = (uint8_t)*ticks_since_boot;
 
-    serial_send( 0xfe ); // start packet
+        serial_send( 0xfe ); // start packet
 
-    serial_send( 4 ); // num args
-    enc_serial_send( (uint8_t)(id >> 8) );
-    enc_serial_send( (uint8_t)id );
-    enc_serial_send( tick );
-    enc_serial_send( microtick );
+        serial_send( 4 ); // num args
+        enc_serial_send( (uint8_t)(id >> 8) );
+        enc_serial_send( (uint8_t)id );
+        enc_serial_send( tick );
+        enc_serial_send( microtick );
 
-    enc_serial_send( (uint8_t)(a >> 8) );
-    enc_serial_send( (uint8_t)a );
+        enc_serial_send( (uint8_t)(a >> 8) );
+        enc_serial_send( (uint8_t)a );
 
-    enc_serial_send( (uint8_t)(b >> 8) );
-    enc_serial_send( (uint8_t)b );
+        enc_serial_send( (uint8_t)(b >> 8) );
+        enc_serial_send( (uint8_t)b );
 
-    enc_serial_send( (uint8_t)(c >> 8) );
-    enc_serial_send( (uint8_t)c );
+        enc_serial_send( (uint8_t)(c >> 8) );
+        enc_serial_send( (uint8_t)c );
 
-    enc_serial_send( (uint8_t)(d >> 8) );
-    enc_serial_send( (uint8_t)d );
+        enc_serial_send( (uint8_t)(d >> 8) );
+        enc_serial_send( (uint8_t)d );
+    }
 }
 
 
@@ -162,31 +173,33 @@ void print5( uint16_t id,
              uint16_t d,
              uint16_t e )
 {
-    uint8_t microtick = TCNT0 + CYCLES_PER_TICK;
-    uint8_t tick = (uint8_t)*ticks_since_boot;
+    ATOMIC_BLOCK( ATOMIC_RESTORESTATE ) {
+        uint8_t microtick = TCNT0 + CYCLES_PER_TICK;
+        uint8_t tick = (uint8_t)*ticks_since_boot;
 
-    serial_send( 0xfe ); // start packet
+        serial_send( 0xfe ); // start packet
 
-    serial_send( 5 ); // num args
-    enc_serial_send( (uint8_t)(id >> 8) );
-    enc_serial_send( (uint8_t)id );
-    enc_serial_send( tick );
-    enc_serial_send( microtick );
+        serial_send( 5 ); // num args
+        enc_serial_send( (uint8_t)(id >> 8) );
+        enc_serial_send( (uint8_t)id );
+        enc_serial_send( tick );
+        enc_serial_send( microtick );
 
-    enc_serial_send( (uint8_t)(a >> 8) );
-    enc_serial_send( (uint8_t)a );
+        enc_serial_send( (uint8_t)(a >> 8) );
+        enc_serial_send( (uint8_t)a );
 
-    enc_serial_send( (uint8_t)(b >> 8) );
-    enc_serial_send( (uint8_t)b );
+        enc_serial_send( (uint8_t)(b >> 8) );
+        enc_serial_send( (uint8_t)b );
 
-    enc_serial_send( (uint8_t)(c >> 8) );
-    enc_serial_send( (uint8_t)c );
+        enc_serial_send( (uint8_t)(c >> 8) );
+        enc_serial_send( (uint8_t)c );
 
-    enc_serial_send( (uint8_t)(d >> 8) );
-    enc_serial_send( (uint8_t)d );
+        enc_serial_send( (uint8_t)(d >> 8) );
+        enc_serial_send( (uint8_t)d );
 
-    enc_serial_send( (uint8_t)(e >> 8) );
-    enc_serial_send( (uint8_t)e );
+        enc_serial_send( (uint8_t)(e >> 8) );
+        enc_serial_send( (uint8_t)e );
+    }
 }
 
 
@@ -198,32 +211,34 @@ void print6( uint16_t id,
              uint16_t e,
              uint16_t f )
 {
-    uint8_t microtick = TCNT0 + CYCLES_PER_TICK;
-    uint8_t tick = (uint8_t)*ticks_since_boot;
+    ATOMIC_BLOCK( ATOMIC_RESTORESTATE ) {
+        uint8_t microtick = TCNT0 + CYCLES_PER_TICK;
+        uint8_t tick = (uint8_t)*ticks_since_boot;
 
-    serial_send( 0xfe ); // start packet
+        serial_send( 0xfe ); // start packet
 
-    serial_send( 6 ); // num args
-    enc_serial_send( (uint8_t)(id >> 8) );
-    enc_serial_send( (uint8_t)id );
-    enc_serial_send( tick );
-    enc_serial_send( microtick );
+        serial_send( 6 ); // num args
+        enc_serial_send( (uint8_t)(id >> 8) );
+        enc_serial_send( (uint8_t)id );
+        enc_serial_send( tick );
+        enc_serial_send( microtick );
 
-    enc_serial_send( (uint8_t)(a >> 8) );
-    enc_serial_send( (uint8_t)a );
+        enc_serial_send( (uint8_t)(a >> 8) );
+        enc_serial_send( (uint8_t)a );
 
-    enc_serial_send( (uint8_t)(b >> 8) );
-    enc_serial_send( (uint8_t)b );
+        enc_serial_send( (uint8_t)(b >> 8) );
+        enc_serial_send( (uint8_t)b );
 
-    enc_serial_send( (uint8_t)(c >> 8) );
-    enc_serial_send( (uint8_t)c );
+        enc_serial_send( (uint8_t)(c >> 8) );
+        enc_serial_send( (uint8_t)c );
 
-    enc_serial_send( (uint8_t)(d >> 8) );
-    enc_serial_send( (uint8_t)d );
+        enc_serial_send( (uint8_t)(d >> 8) );
+        enc_serial_send( (uint8_t)d );
 
-    enc_serial_send( (uint8_t)(e >> 8) );
-    enc_serial_send( (uint8_t)e );
+        enc_serial_send( (uint8_t)(e >> 8) );
+        enc_serial_send( (uint8_t)e );
 
-    enc_serial_send( (uint8_t)(f >> 8) );
-    enc_serial_send( (uint8_t)f );
+        enc_serial_send( (uint8_t)(f >> 8) );
+        enc_serial_send( (uint8_t)f );
+    }
 }
